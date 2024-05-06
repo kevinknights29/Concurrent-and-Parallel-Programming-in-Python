8a243dd027f4d12f02ec5a80b1456dc8afafc393
Kevin Knights <kevinknights29@hotmail.com>
2024-05-06 02:58:57 +0000

    feat: added table schema

---
+++ postgres/scripts/schema.sql
@@ -0,0 +1,8 @@
+--sql
+CREATE TABLE IF NOT EXISTS prices(
+    ticker VARCHAR(25) NOT NULL,
+    price DECIMAL(10, 2) NOT NULL,
+    price_change VARCHAR(25),
+    percentual_change VARCHAR(25),
+    insertion_date TIMESTAMP NOT NULL DEFAULT NOW(),
+)
