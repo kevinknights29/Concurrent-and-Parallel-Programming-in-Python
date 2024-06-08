0d246f4b143676f2956c4ca6fe6697d1976c55cf
Kevin Knights <kevinknights29@hotmail.com>
2024-06-08 21:47:48 +0000

    feat: added PostgresScheduler and postgress queue processing

--- main.py
+++ main.py
@@ -6,11 +6,13 @@ import time
 from multiprocessing import Queue
 from pathlib import Path
 
+from src import postgres
 from src import wikipedia
 from src import yahoo_finance
 
 # Define the number of YahooFinancePriceScheduler instances
 YAHOO_SCHEDULERS = 5
+POSTGRES_SCHEDULERS = 5
 
 # Initialize the logger
 logger = logging.getLogger(__name__)
@@ -42,13 +44,22 @@ def main() -> None:
 
     start_time = time.time()
     tickers_queue: Queue = Queue()
+    postgres_queue: Queue = Queue()
 
     wiki_worker = wikipedia.WikiWorker()
     yahoo_schedulers = []
+    postgres_schedulers = []
 
     for _ in range(YAHOO_SCHEDULERS):
-        scheduler = yahoo_finance.YahooFinancePriceScheduler(tickers_queue)
-        yahoo_schedulers.append(scheduler)
+        yahoo_scheduler = yahoo_finance.YahooFinancePriceScheduler(
+            input_queue=tickers_queue,
+            output_queue=postgres_queue,
+        )
+        yahoo_schedulers.append(yahoo_scheduler)
+
+    for _ in range(POSTGRES_SCHEDULERS):
+        postgres_scheduler = postgres.PostgresScheduler(postgres_queue)
+        postgres_schedulers.append(postgres_scheduler)
 
     for company_ticker in wiki_worker.get_page_content():
         ticker = company_ticker[0]
