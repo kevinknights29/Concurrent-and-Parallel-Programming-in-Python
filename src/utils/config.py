b8f2068e327c49b8c16e57627b6d28a96944b12c
Kevin Knights <kevinknights29@hotmail.com>
2024-06-14 23:44:38 +0000

    feat: added config.py within the utils submodule

---
+++ src/utils/config.py
@@ -0,0 +1,30 @@
+from __future__ import annotations
+
+import logging
+
+import yaml  # type: ignore
+from yaml.loader import SafeLoader  # type: ignore
+
+from src.utils import constants
+
+# Initialize the logger
+logger = logging.getLogger(__name__)
+
+conf: dict = {}
+
+
+def config() -> dict:
+    """
+    Loads the config.yaml file and returns a dict stored in config
+
+    Returns:
+        dict: configuration dictionary
+    """
+
+    global conf
+    if not conf:
+        logger.debug("Reading from config file.")
+        with open(constants.CONFIG_FILE, encoding="utf-8") as cfg:
+            conf = yaml.load(cfg, Loader=SafeLoader)
+    logger.info("Config loaded.")
+    return conf
